6d13e4be747bf13a0b7e4a77ca8b846178eb80ae
haolei <h00374456@china.huawei.com>
2016-12-23 11:59:09 +0800

    支持扫描tag，并生成url参数

--- encode.go
+++ encode.go
@@ -1,10 +1,11 @@
 package tags
 
 import (
+	"bytes"
 	"fmt"
-	"reflect"
-	"strings"
 	"net/url"
+	"reflect"
+	"strconv"
 )
 
 type URLValue struct {
@@ -13,30 +14,24 @@ type URLValue struct {
 
 func (u *URLValue) Encode(a interface{}) string {
 	v := reflect.ValueOf(a)
-
-	fmt.Println(v.Kind())
-	
-
 	p := &printer{
 		tagname: u.TagName,
-		kvs:make(url.Values),
+		kvs:     make(url.Values),
 		visited: make(map[visit]int),
-		depth: 0,
+		depth:   0,
 	}
 	p.printValue(v)
 
 	return p.encode()
 }
 
-// printValue must keep track of already-printed pointer values to avoid
-// infinite recursion.
 type visit struct {
 	v   uintptr
 	typ reflect.Type
 }
 
 type printer struct {
-	key     []string
+	key     bytes.Buffer
 	kvs     url.Values
 	tagname string
 	visited map[visit]int
@@ -44,37 +39,35 @@ type printer struct {
 }
 
 func (p *printer) encode() string {
-	return	p.kvs.Encode()
+	return p.kvs.Encode()
 }
 
 func (p *printer) printValue(v reflect.Value) {
-	fmt.Println(p.depth, v.Kind(), v.Interface(), p.kvs, (p.key))
 	if p.depth > 10 {
 		return
 	}
 
 	switch v.Kind() {
 	case reflect.Bool:
-		fallthrough
+		p.kvs.Set(p.key.String(), strconv.FormatBool(v.Bool()))
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		fallthrough
+		p.kvs.Set(p.key.String(), strconv.FormatInt(v.Int(), 10))
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		fallthrough
+		p.kvs.Set(p.key.String(), strconv.FormatUint(v.Uint(), 10))
 	case reflect.Float32, reflect.Float64:
-		fallthrough
+		p.kvs.Set(p.key.String(), strconv.FormatFloat(v.Float(), 'G', -1, 64))
 	case reflect.Complex64, reflect.Complex128:
-		fallthrough
+		p.kvs.Set(p.key.String(), fmt.Sprint(v.Interface()))
 	case reflect.String:
-		p.kvs.Set(strings.Join(p.key, "."), fmt.Sprint(v.Interface()))
+		p.kvs.Set(p.key.String(), v.String())
 	case reflect.Map:
-		if nonzero(v) {
-			keys := v.MapKeys()
-			for i := 0; i < v.Len(); i++ {
-				k := keys[i]
-				mv := v.MapIndex(k)
-				p.key = append(p.key, fmt.Sprint(k))
-				p.printValue(mv)
-			}
+		keys := v.MapKeys()
+		for i := 0; i < v.Len(); i++ {
+			k := keys[i]
+			n := p.key.Len()
+			p.printKey(fmt.Sprint(k), n)
+			p.printValue(v.MapIndex(k))
+			p.key.Truncate(n)
 		}
 	case reflect.Struct:
 		t := v.Type()
@@ -87,24 +80,26 @@ func (p *printer) printValue(v reflect.Value) {
 			p.visited[vis] = p.depth
 		}
 
-		if nonzero(v) {
-			for i := 0; i < v.NumField(); i++ {
-				f := t.Field(i)
-				if tag, ok := f.Tag.Lookup(p.tagname); ok && tag != "" {
-					n := len(p.key)
-					p.key = append(p.key, tag)
-					p.printValue(getField(v, i))
-					p.key = p.key[0:n]
-				}
+		for i := 0; i < v.NumField(); i++ {
+			f := t.Field(i)
+			if tag, ok := f.Tag.Lookup(p.tagname); ok && tag != "" {
+				n := p.key.Len()
+				p.printKey(tag, n)
+				p.printValue(getField(v, i))
+				p.key.Truncate(n)
 			}
 		}
+
 	case reflect.Array, reflect.Slice:
 		for i := 0; i < v.Len(); i++ {
-			n:=len(p.key)
-			p.key = append(p.key, fmt.Sprintf("[%d]", i))
+			n := p.key.Len()
+			p.key.WriteByte('[')
+			p.key.WriteString(strconv.Itoa(i))
+			p.key.WriteByte(']')
 			p.printValue(v.Index(i))
-			p.key = p.key[0:n]
+			p.key.Truncate(n)
 		}
+
 	case reflect.Ptr:
 		e := v.Elem()
 		if e.IsValid() {
@@ -115,92 +110,19 @@ func (p *printer) printValue(v reflect.Value) {
 	}
 }
 
-//
-//
-//func (p *printer) printInline(v reflect.Value, x interface{}) {
-//	fmt.Fprintf(p, "(%v)", x)
-//}
+func (p *printer) printKey(key string, n int) {
+	if n > 0 {
+		p.key.WriteByte('.')
+	}
+	p.key.WriteString(key)
+}
 
 func getField(v reflect.Value, i int) reflect.Value {
 	val := v.Field(i)
+
 	if val.Kind() == reflect.Interface && !val.IsNil() {
 		val = val.Elem()
 	}
+
 	return val
-}
-
-func nonzero(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Bool:
-		return v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() != 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() != 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() != 0
-	case reflect.Complex64, reflect.Complex128:
-		return v.Complex() != complex(0, 0)
-	case reflect.String:
-		return v.String() != ""
-	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			if nonzero(getField(v, i)) {
-				return true
-			}
-		}
-		return false
-	case reflect.Array:
-		for i := 0; i < v.Len(); i++ {
-			if nonzero(v.Index(i)) {
-				return true
-			}
-		}
-		return false
-	case reflect.Map, reflect.Interface, reflect.Slice, reflect.Ptr, reflect.Chan, reflect.Func:
-		return !v.IsNil()
-	case reflect.UnsafePointer:
-		return v.Pointer() != 0
-	}
-	return true
-}
-
-func labelType(t reflect.Type) bool {
-	switch t.Kind() {
-	case reflect.Interface, reflect.Struct:
-		return true
-	}
-	return false
-}
-func canInline(t reflect.Type) bool {
-	switch t.Kind() {
-	case reflect.Map:
-		return !canExpand(t.Elem())
-	case reflect.Struct:
-		for i := 0; i < t.NumField(); i++ {
-			if canExpand(t.Field(i).Type) {
-				return false
-			}
-		}
-		return true
-	case reflect.Interface:
-		return false
-	case reflect.Array, reflect.Slice:
-		return !canExpand(t.Elem())
-	case reflect.Ptr:
-		return false
-	case reflect.Chan, reflect.Func, reflect.UnsafePointer:
-		return false
-	}
-	return true
-}
-
-func canExpand(t reflect.Type) bool {
-	switch t.Kind() {
-	case reflect.Map, reflect.Struct,
-		reflect.Interface, reflect.Array, reflect.Slice,
-		reflect.Ptr:
-		return true
-	}
-	return false
 }
--- encode_test.go
+++ encode_test.go
@@ -1,47 +1,77 @@
 package tags
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+)
 
 type S2 struct {
-	SF1 int `mts:"sf1"`
-
-	Data []int `mts:"data"`
+	SF1  int   `tag:"sf1"`
+	Data []int `tag:"data"`
 }
 
 type MockS struct {
-	Field1 int            `mts:"int"`
-	Field2 string         `mts:"string"`
-	Field3 S2             `mts:"struct"`
-	Field4 map[string]int `mts:"map"`
-	Field5 []S2           `mts:"structPtr"`
+	Field1 int            `tag:"int"`
+	Field2 string         `tag:"string"`
+	Field3 S2             `tag:"struct"`
+	Field4 map[string]int `tag:"map"`
+	Field5 []S2           `tag:"structArray"`
+	Field6 *S2            `tag:"structPtr"`
+	Field7 *S2            `tag:"structPtr"`
+}
+
+var data = MockS{
+	Field1: 1,
+	Field2: "test",
+	Field3: S2{
+		SF1:  2,
+		Data: []int{10, 11, 12},
+	},
+	Field4: map[string]int{
+		"map1": 100,
+		"map0": 99,
+	},
+	Field5: []S2{
+		S2{
+			SF1:  2,
+			Data: []int{20, 21},
+		},
+		S2{
+			SF1:  3,
+			Data: []int{30, 31},
+		},
+	},
+	Field6: &S2{
+		SF1:  4,
+		Data: []int{41, 42},
+	},
 }
 
 func TestEncode(t *testing.T) {
-	s := MockS{
-		Field1: 1,
-		Field2: "test",
-		Field3: S2{
-			SF1:  2,
-			Data: []int{10, 11, 12},
-		},
-		Field4: map[string]int{
-			"map1": 100,
-			"map0": 99,
-		},
-		Field5: []S2{
-			S2{
-				SF1:  2,
-				Data: []int{20, 21},
-			},
-			S2{
-				SF1:  3,
-				Data: []int{30, 31},
-			},
-		},
+	u := URLValue{TagName: "tag"}
+	str := u.Encode(&data)
+
+	values, err := url.ParseQuery(str)
+	if err != nil {
+		t.Error(err)
+		return
 	}
 
-	u := URLValue{TagName: "mts"}
-	str := u.Encode(&s)
+	for k, v := range values {
+		t.Logf("%s=%s\n", k, v)
+	}
+}
 
+func BenchmarkURLValue_Encode(b *testing.B) {
+	u := URLValue{TagName: "tag"}
+	for i := 0; i < b.N; i++ {
+		_ = u.Encode(data)
+	}
+}
+
+func TestURLValue_Encode1(t *testing.T) {
+	u := URLValue{TagName: "tag"}
+	a := 1
+	str := u.Encode(a)
 	t.Log(str)
 }
